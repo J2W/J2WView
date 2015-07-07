5899983fcc53cbc408e383135b0248af9d48c430
skyJinc <jincan0213@hotmail.com>
2015-07-07 16:53:00 +0800

    View层分离

---
+++ app/src/main/java/j2w/team/view/widget/materialWidget/style/IMaterial.java
@@ -0,0 +1,10 @@
+package j2w.team.view.widget.materialWidget.style;
+
+public interface IMaterial {
+	public void perfirmSuperClick();
+
+	public void setColor(int fullColor);
+
+	public void setType(int widgetType);
+
+}
